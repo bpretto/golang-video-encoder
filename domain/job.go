eb98c39fd43b28079055eaed25d9447a873150ba
Bernardo Pretto <bernardo@pretto.dev>
2025-02-25 21:14:56 -0300

    add: job tests and validations

--- domain/job.go
+++ domain/job.go
@@ -1,13 +1,53 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
 
 type Job struct {
-	ID               string
-	OutputBucketPath string
-	Status           string
-	Video            *Video // This is a pointer to the video object
-	Error            string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+	ID               string    `valid:"uuidv4"`
+	OutputBucketPath string    `valid:"notnull"`
+	Status           string    `valid:"notnull, in(pending|ongoing|completed|failed)"`
+	Video            *Video    `valid:"-"`
+	Error            string    `valid:"-"`
+	CreatedAt        time.Time `valid:"-"`
+	UpdatedAt        time.Time `valid:"-"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func NewJob(outputBucketPath string, video *Video) (*Job, error) {
+	job := Job{
+		OutputBucketPath: outputBucketPath,
+		Video:            video,
+		Status:           "pending",
+	}
+
+	job.prepare()
+
+	err := job.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &job, nil
+}
+
+func (job *Job) prepare() {
+	job.ID = uuid.NewV4().String()
+	job.CreatedAt = time.Now()
+	job.UpdatedAt = time.Now()
+}
+
+func (job *Job) Validate() error {
+	_, err := govalidator.ValidateStruct(job)
+	if err != nil {
+		return err
+	}
+	return nil
 }
