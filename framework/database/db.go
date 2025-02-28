b4203cc323ce4f4bbe45932ff80a4f95c78266fa
Bernardo Pretto <bernardo@pretto.dev>
2025-02-27 22:24:45 -0300

    add: database

---
+++ framework/database/db.go
@@ -0,0 +1,66 @@
+package database
+
+import (
+	"encoder/domain"
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	Db            *gorm.DB
+	Dsn           string
+	DsnTest       string
+	DbType        string
+	DbTypeTest    string
+	Debug         bool
+	AutoMigrateDb bool
+	Env           string
+}
+
+func NewDatabase() *Database {
+	return &Database{}
+}
+
+func NewDatabaseTest() *gorm.DB {
+	dbInstance := NewDatabase()
+	dbInstance.Env = "test"
+	dbInstance.DbTypeTest = "sqlite3"
+	dbInstance.DsnTest = ":memory:"
+	dbInstance.AutoMigrateDb = true
+	dbInstance.Debug = true
+
+	connection, err := dbInstance.Connect()
+
+	if err != nil {
+		log.Fatal("Cannot connect to database")
+	}
+
+	return connection
+}
+
+func (d *Database) Connect() (*gorm.DB, error) {
+	var err error
+
+	if d.Env == "test" {
+		d.Db, err = gorm.Open(d.DbTypeTest, d.DsnTest)
+	} else {
+		d.Db, err = gorm.Open(d.DbType, d.Dsn)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if d.Debug {
+		d.Db.LogMode(true)
+	}
+
+	if d.AutoMigrateDb {
+		d.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
+	}
+
+	return d.Db, nil
+}
